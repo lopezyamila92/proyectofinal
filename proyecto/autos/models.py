5cec89e5c4bc3ca6f883d25847e10035844cb80d
lopezyamila92 <lopez.yamila92@icloud.com>
2022-07-31 08:57:08 -0300

    products

--- proyecto/autos/models.py
+++ proyecto/autos/models.py
@@ -7,6 +7,7 @@ class Autos(models.Model):
     description = models.CharField(max_length=200, null=True, blank=True)
     is_active = models.BooleanField(default=True)
     creation_date = models.DateField(auto_now_add=True, null=True, blank=True)
+    stock = models.IntegerField()
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
--- proyecto/autos/views.py
+++ proyecto/autos/views.py
@@ -1,3 +1,23 @@
+from enum import auto
+from hashlib import new
+from itertools import product
+from multiprocessing import context
 from django.shortcuts import render
+from autos.models import Autos
 
-# Create your views here.
+
+def create_autos(request):
+    create_autos = Autos.objects.create(name = "ford falcon", price = 100, stock = 15)
+    context = {
+        "new_product": create_autos
+    }
+    return render(request, "new_product.html", context=context)
+
+def list_autos(request):
+    autos = Autos.objects.all()
+    context = {
+        "autos": autos
+    } 
+    return render(request, "products_list.html", context=context)
+
+
