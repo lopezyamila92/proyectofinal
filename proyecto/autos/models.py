2b67e0f6f2bd2e2fad5aaf284e9352984053c44b
lopezyamila92 <lopez.yamila92@icloud.com>
2022-07-31 09:48:47 -0300

    servicios

--- proyecto/autos/models.py
+++ proyecto/autos/models.py
@@ -11,3 +11,10 @@ class Autos(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+class servicio(models.Model):
+    name = models.CharField(max_length=40)
+    price = models.FloatField()
+    description = models.CharField(max_length=200, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    creation_date = models.DateField(auto_now_add=True, null=True, blank=True)
--- proyecto/autos/views.py
+++ proyecto/autos/views.py
@@ -7,16 +7,16 @@ from autos.models import Autos
 
 
 def create_autos(request):
-    create_autos = Autos.objects.create(name = "ford falcon", price = 100, stock = 15)
+    create_autos = Autos.objects.create(name = "ford ranger", price = 1000, stock = 30)
     context = {
         "new_product": create_autos
     }
     return render(request, "new_product.html", context=context)
 
 def list_autos(request):
-    inventario = Autos.objects.all()
+    products = Autos.objects.all()
     context = {
-        "inventario": inventario
+        "products": products
     } 
     return render(request, "products_list.html", context=context)
 
