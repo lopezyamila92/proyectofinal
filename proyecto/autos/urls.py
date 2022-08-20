216ee730d3f9280b4e4f4985555a2d8083d92663
lopezyamila92 <lopez.yamila92@icloud.com>
2022-08-20 14:33:00 -0300

    app21

--- proyecto/autos/urls.py
+++ proyecto/autos/urls.py
@@ -3,7 +3,7 @@ from .views import List_articles
 from proyecto.views import categorias
 from proyecto.views import inicio
 from autos.views import create_autos , list_autos , servicio , inicio , primer_formulario, search_products, delete_product , update_product, List_articles
-    
+from servicio.views import create_service , list_service
  
 
 
@@ -13,12 +13,14 @@ urlpatterns = [
     path('categorias/', categorias, name="categorias"),
     path('create_autos/', create_autos, name = "create_autos"),
     path('list_autos/', list_autos, name = "list_autos"),
-    path('servicio/', servicio, name="servicio"),
+    path('servicio1/', create_service, name="create_service"),
     path('primer-formulario/', primer_formulario, name="primer-formulario"),
     path('search-products/', search_products, name="search_products"),
     path("delete-product/<int:pk>/", delete_product, name="delete_product"),
     path("update-product/<int:pk>/", update_product, name="update_product"),
     path("list-articles/", List_articles.as_view(), name="List_articles"),
+    path('servicio/', list_service, name="list_service"),
+
 ]
 
 
